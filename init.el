5a4756e0efbed3c7bb2b4c5101a410c1d842334f
Luke Albao <lukealbao@gmail.com>
2015-02-01 19:31:37 -0500

    modularize

--- init.el
+++ init.el
@@ -48,59 +48,31 @@
 (provide 'better-defaults)
 ;;; better-defaults.el ends here
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(column-number-mode t)
- '(custom-enabled-themes (quote (tango-dark)))
- '(custom-safe-themes
-   (quote
-    ("084fb2f46f170f79b68db062e7d47df904739e4846bf4cea742dbf66ceef672a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "943bff6eada8e1796f8192a7124c1129d6ff9fbd1a0aed7b57ad2bf14201fdd4" default)))
- '(fci-rule-color "#383838")
- '(tool-bar-mode nil)
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo")))))
+;; == Here Be Personal Settings ==
 
-;; PERSONAL SETTINGS HERE!
+;; Paths
+(setq themes-dir
+       (expand-file-name "themes" user-emacs-directory))
+(setq packages-dir
+      (expand-file-name "packages" user-emacs-directory))
+(setq settings-dir
+       (expand-file-name "settings" user-emacs-directory))
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(add-to-list 'load-path "~/.emacs.d/packages/")
+(add-to-list 'custom-theme-load-path themes-dir)
+(add-to-list 'load-path packages-dir)
+(add-to-list 'load-path settings-dir)
+
+;; Get the stuff set by Custom
+(setq custom-file
+      (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+;; Appearance-type stuff
+(require 'appearance)
 
 ;; Interactive JavaScript
-(add-to-list 'load-path "~/.emacs.d/packages/js-comint/")
 (require 'js-comint)
+
 ;; Remove weird prompt codes
 (setenv "NODE_NO_READLINE" "1")
 (setq inferior-js-program-command "/usr/local/bin/node --interactive")
